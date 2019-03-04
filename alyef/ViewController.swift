92ea1b9304ca6c2ad679bacb7f6baefd907a0509
awad <awad619@hotmail.com>
2019-03-04 17:19:03 +0200

    i nor it

--- alyef/ViewController.swift
+++ alyef/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var last:String = "te"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
